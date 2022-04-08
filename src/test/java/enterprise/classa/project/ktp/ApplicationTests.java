8f445bee57425e3acb163e54e65d40da99297af3
AnggitaTyasmara <anggitotsmr@gmail.com>
2022-04-08 15:43:01 +0700

    Project KTP

---
+++ src/test/java/enterprise/classa/project/ktp/ApplicationTests.java
@@ -0,0 +1,13 @@
+package enterprise.classa.project.ktp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
