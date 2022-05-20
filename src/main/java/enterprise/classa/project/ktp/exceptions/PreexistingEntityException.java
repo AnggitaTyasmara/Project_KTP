d70231e0b0f70b55dacaeedf23192b59b125d61d
AnggitaTyasmara <anggitotsmr@gmail.com>
2022-05-20 19:45:11 +0700

    update data controller dan dummy

---
+++ src/main/java/enterprise/classa/project/ktp/exceptions/PreexistingEntityException.java
@@ -0,0 +1,10 @@
+package enterprise.classa.project.ktp.exceptions;
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
